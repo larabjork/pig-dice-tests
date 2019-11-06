7c1ff8acf4bae3ffafb494579625e2cd0e313569
Student <example@example.com>
2019-11-06 11:57:30 -0800

    add roll button

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,22 @@ Game.prototype.addPlayer = function () {
 
 
 // Business Logic for Player
-function Player(playerName) {
+function Player(playerName, tempTotal, permTotal) {
   this.playerName = playerName,
   this.tempTotal = 0,
   this.permTotal = 0;
 }
 
 Player.prototype.roll = function () {
+  if (permTotal>=100) {
+    alert("Winner!")
+  } else {
+    var rand = 1 + Math.floor(Math.random() * 6);
 
-};
+
+    }
+
+  };
 
 Player.prototype.hold = function() {
 
@@ -32,6 +39,8 @@ Player.prototype.hold = function() {
 
 
 // UI Logic
+var game = new Game();
+
 
 $(document).ready(function() {
   $("form#playerInfo").submit(function(event){
@@ -46,6 +55,15 @@ $(document).ready(function() {
 
     $("#player1Val").text(player1NameInput);
     $("#player2Val").text(player2NameInput);
+    // $("#player1temp").append("eva");
+    // $("#player2temp").append("pete")
 console.log(newPlayer1,newPlayer2);
   });
+  $("button#roll").click(function(){
+    var rand = 1 + Math.floor(Math.random() * 6)
+    var tempTotal = 0
+    var total = rand + tempTotal
+    $("li#player2temp").append(total);
+    console.log(rand);
+  })
 });
