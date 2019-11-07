4e0e0dd2217d4abbc5e5db90fa5b72e025966bda
Student <example@example.com>
2019-11-07 14:30:41 -0800

    winnerWinner function working

--- js/scripts.js
+++ js/scripts.js
@@ -15,85 +15,90 @@ function Player(playerName) {
 }
 
 Player.prototype.rollDice = function () {
+  winnerWinner(this);
+
   // if (permTotal>=100) {
-  //   alert("Winner!")
-  // } else {
-    var roll = 1 + Math.floor(Math.random() * 6);
-    if (this.tempTotal + this.permTotal >= 100){
-      alert("you win")
-    } else if (roll !== 1) {
-      this.tempTotal = this.tempTotal + roll;
-    } else {
-    switchDisplay();
-  }
-  };
-
-Player.prototype.hold = function() {
-  this.permTotal = this.tempTotal + this.permTotal
-  this.tempTotal = 0
-};
-
-// Player.prototype.
+    //   alert("Winner!")
+    // } else {
+      var roll = 1 + Math.floor(Math.random() * 6);
+      if (this.tempTotal + this.permTotal >= 10){
+        // alert("you win")
+      } else if (roll !== 1) {
+        this.tempTotal = this.tempTotal + roll;
+        winnerWinner(this);
 
 
+      } else {
+        switchDisplay();
+        winnerWinner(this);
+
+      }
+    };
+
+    Player.prototype.hold = function() {
+      winnerWinner(this);
+      this.permTotal = this.tempTotal + this.permTotal
+      this.tempTotal = 0
+      switchDisplay();
+    };
 
 
+    // UI Logic
+    var game = new Game();
 
-// UI Logic
-var game = new Game();
-
-function switchDisplay() {
-  $("#player2deets").toggle();
-  $("#player1deets").toggle();
-
-}
-
-$(document).ready(function() {
-  var newPlayer1 = "ricki";
-  var newPlayer2 = "Jamie";
-
-  $("form#playerInfo").submit(function(event){
-
-    event.preventDefault();
+    function switchDisplay() {
+      $("#player2deets").toggle();
+      $("#player1deets").toggle();
+    }
+    function winnerWinner(player) {
+      if (player.tempTotal + player.permTotal >= 10){
+        alert("you win");
+        $("#intro").show();
+        $("#game").hide();
+      }
+    }
 
 
-    var player1NameInput = $("input#player1").val();
-    var player2NameInput = $("input#player2").val();
-    newPlayer1 = new Player(player1NameInput);
-    newPlayer2 = new Player(player2NameInput);
-    // console.log(newPlayer1);
-    $("#intro").hide();
-    $("#game").show();
+    $(document).ready(function() {
+      var newPlayer1 = "ricki";
+      var newPlayer2 = "Jamie";
 
+      $("form#playerInfo").submit(function(event){
 
-    $("#player1Val").text(player1NameInput);
-    $("#player2Val").text(player2NameInput);
-    console.log(newPlayer1);
-  });
-  $("button#roll1").click(function(){
-    newPlayer1.rollDice();
-    $("#player1temp").append('<li>' + newPlayer1.tempTotal + '</li>');
-  })
-  $("button#hold1").click(function(){
-    newPlayer1.hold();
-    $("#player1Total").text(newPlayer1.permTotal);
-    $("#player1temp").hide();
-    $("#player2deets").show();
-    $("#player1deets").hide();
-    $("#player2temp").show();
+        event.preventDefault();
 
-  })
-  $("button#roll2").click(function(){
-    newPlayer2.rollDice();
-    $("#player2temp").append('<li>' + newPlayer2.tempTotal + '</li>');
-  })
-  $("button#hold2").click(function(){
-    newPlayer2.hold();
-    $("#player2Total").text(newPlayer2.permTotal);
-    $("#player2temp").hide();
-    $("#player1temp").show();
-    $("#player1deets").show();
-    $("#player2deets").hide();
+        var player1NameInput = $("input#player1").val();
+        var player2NameInput = $("input#player2").val();
+        newPlayer1 = new Player(player1NameInput);
+        newPlayer2 = new Player(player2NameInput);
+        // console.log(newPlayer1);
+        $("#intro").hide();
+        $("#game").show();
 
-  })
-});
+        $("#player1Val").text(player1NameInput);
+        $("#player2Val").text(player2NameInput);
+        console.log(newPlayer1);
+      });
+      $("button#roll1").click(function(){
+        newPlayer1.rollDice();
+        $("#player1temp").text(newPlayer1.tempTotal);
+      })
+      $("button#hold1").click(function(){
+        newPlayer1.hold();
+        $("#player1Total").text(newPlayer1.permTotal);
+        // switchTemp();
+        // switchDisplay();
+      })
+      $("button#roll2").click(function(){
+        newPlayer2.rollDice();
+        // $("#player2temp").empty();
+        $("#player2temp").text(newPlayer2.tempTotal);
+      })
+      $("button#hold2").click(function(){
+        newPlayer2.hold();
+        $("#player2Total").text(newPlayer2.permTotal);
+        // switchTemp();
+        // switchDisplay();
+
+      })
+    });
